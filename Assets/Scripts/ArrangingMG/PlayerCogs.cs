7001e7080968114d747660c3b7f2f87a1b880871
PeachyMallow <rebecca.abby.hughes@gmail.com>
2022-08-28 19:50:57 +0100

    Tidied Up ArrangingMG

--- Assets/Scripts/ArrangingMG/PlayerCogs.cs
+++ Assets/Scripts/ArrangingMG/PlayerCogs.cs
@@ -18,24 +18,21 @@ public class PlayerCogs : MonoBehaviour, IPointerDownHandler, IBeginDragHandler,
     {
         canvasGroup.alpha = .6f;
         canvasGroup.blocksRaycasts = false;
-        Debug.Log("OnBeginDrag");
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         rectTransform.anchoredPosition += eventData.delta;
-        Debug.Log("OnDrag");
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         canvasGroup.alpha = 1f;
         canvasGroup.blocksRaycasts = true;
-        Debug.Log("OnEndDrag");
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("OnPointerDown");
+       
     }
 }
