2e21d5dc06fce5bd20d01ca8b9b4ba1923f74cb9
Zbordek - Wiktor Zborowski <61721211+wzGCU@users.noreply.github.com>
2022-08-29 19:11:42 +0200

    Welding as 2 minigames (finish 1st one ti get to drawing)

---
+++ Assets/WeldingTask/WeldTorch.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeldTorch : MonoBehaviour
+{
+    public int puzzleTotalCount;
+    public GameObject win;
+    private int puzzleCount = 0;
+    public Sprite sprite, highlightSprite;
+
+    static public WeldTorch Instance;
+    private void Awake()
+    {
+        Instance = this;
+    }
+    public void PuzzleAddPoint()
+    {
+        puzzleCount++;
+    }
+
+    private void OnMouseDown()
+    {
+        if (puzzleCount == puzzleTotalCount)
+        {
+            Debug.Log("finished minigame 1");
+            Instantiate(win, transform.parent.transform.position, transform.parent.transform.rotation, transform.parent.transform.parent);
+            Destroy(transform.parent.gameObject);
+        }
+        else
+        {
+            Debug.Log("not all pieces done. currently at: " + puzzleCount);
+        }
+    }
+    void OnMouseOver()
+    {
+        transform.GetComponent<SpriteRenderer>().sprite = highlightSprite;
+    }
+
+    void OnMouseExit()
+    {
+        transform.GetComponent<SpriteRenderer>().sprite = sprite;
+    }
+}
---
+++ Assets/WeldingTask/Weld_Broken.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weld_Broken : MonoBehaviour
+{
+    bool clickable = true;
+    public Sprite sprite, highlightSprite;
+    void OnMouseDrag()
+    {
+        if (clickable)
+        {
+            //getting the raycast
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 newpos = new Vector3();
+            if (Physics.Raycast(ray, out RaycastHit raycastHit, 100000f))
+            {
+
+                newpos = new Vector3(raycastHit.point.x, raycastHit.point.y, transform.position.z);
+
+            }
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(newpos, .0001f);
+            foreach (Collider2D collider in colliders)
+            {
+                //make sure not own collider
+                if (collider.gameObject != gameObject && collider.gameObject.transform.parent.gameObject.name == "slots")
+                {
+                    transform.position = collider.transform.position;
+                    collider.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+                    WeldTorch.Instance.PuzzleAddPoint();
+                    clickable=false;
+                    return;
+                }
+            }
+            transform.position = newpos;
+        }
+        
+    }
+    void OnMouseOver()
+    {
+        if (clickable)
+        {
+            transform.GetComponent<SpriteRenderer>().sprite = highlightSprite;
+        }
+  
+    }
+
+    void OnMouseExit()
+    {
+        transform.GetComponent<SpriteRenderer>().sprite = sprite;
+    }
+
+}
